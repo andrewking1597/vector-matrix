5ca5d3bd4e3c47b2aedecf72301df0dcd217f139
Andrew King <andrewking1597@gmail.com>
2021-06-17 12:29:02 -0400

    CoefficientMatrix class, fix ref method

--- matrix.py
+++ matrix.py
@@ -1,60 +1,35 @@
 from row import Row
 
-class Matrix:
-    def __init__(self, num_rows, num_cols, is_augmented=False):
-        self.NUM_ROWS = num_rows
-        self.NUM_COLS = num_cols
-        self.IS_AUGMENTED = is_augmented
-        self.ROWS = []
-
-    def rref(self):
-        self.ref()
-
-        # for each row, starting with the last row and ending with the second row
-        for i in range(self.NUM_ROWS-1, 0, -1):
-            # reduce the row so the pivot value is 1
-            self.ROWS[i].reduce()
-            # zeros above
-            self._zeros_above(i)
-            self._sort_rows()
-
-        # reduce row 0
-        self.ROWS[0].reduce()
+class CoefficientMatrix:
+    def __init__(self, data):
+        self.ROWS = [Row(d) for d in data]
+        self.NUM_ROWS = len(data)
+        self.NUM_COLS = len(data[0]) # assuming all rows same length (todo exception handling)
 
     def ref(self):
         self._sort_rows()
-        for i in range(self.NUM_ROWS - 1):
-            self._zeros_below(i)
+
+        current_row = 0
+        while current_row < self.NUM_ROWS - 1 and self.ROWS[current_row].update_pivot_pos() < self.NUM_COLS:
+            self._zeros_below(current_row)
             self._sort_rows()
-        return
-
-    def _zeros_above(self, row_num):
-        # get the relevant pivot position and pivot value
-        pivot_pos = self.ROWS[row_num].get_pivot_pos()
-        pivot_value = self.ROWS[row_num].get_value(pivot_pos)
-
-        # loop through the rows ABOVE row_num
-        for i in range(row_num-1, -1, -1):
-            # calculate the appropriate scalar
-            current_scalar = -1 * self.ROWS[i].get_value(pivot_pos) / pivot_value
-            # scale and replace scalar, row_num, i
-            self._scale_and_replace(current_scalar, row_num, i)
-
+            current_row += 1
         return
 
     def _zeros_below(self, row_num):
         # get the relevant pivot position and pivot value
         pivot_pos = self.ROWS[row_num].get_pivot_pos()
-        pivot_value = self.ROWS[row_num].get_value(pivot_pos)
 
+        pivot_value = self.ROWS[row_num].get_value(pivot_pos)
+        
         # loop through the rows BELOW row_num
         for i in range(row_num+1, self.NUM_ROWS):
             # calculate the appropriate scalar
             current_scalar = -1 * self.ROWS[i].get_value(pivot_pos) / pivot_value
             # scale and replace scalar, row_num, i
             self._scale_and_replace(current_scalar, row_num, i)
-
-        return
+            
+        return True
 
     def _scale_and_replace(self, scalar, row_a, row_b):
         """ (scalar)(row_a) + row_b --> row_b """
@@ -88,27 +63,6 @@ class Matrix:
 
         return
 
-    def add_row(self, row_list):
-        """ create a new row and append it to the matrix """
-
-        # use row_list to make a Row object
-        row = Row(row_list)
-
-        if row.get_length() == self.NUM_COLS and len(self.ROWS) < self.NUM_ROWS:
-            self.ROWS.append(row)
-            return 1 # success
-        elif len(self.ROWS) >= self.NUM_ROWS:
-            print("Error: matrix full")
-            return 0 # error
-        elif row.get_length() != self.NUM_COLS:
-            print("Error: rows must have a length of {}.".format(self.NUM_COLS))
-            return 0 # error
-
-    def add_rows(self, rows):
-        """ take a list of lists and add each as a row """
-        for row in rows:
-            self.add_row(row)
-
     def print_matrix(self):
         for r in self.ROWS:
             r.print_row()
@@ -120,8 +74,6 @@ class Matrix:
         return self.NUM_ROWS
     def get_num_cols(self):
         return self.NUM_COLS
-    def get_is_augmented(self):
-        return self.IS_AUGMENTED
     def get_rows(self):
         return self.ROWS
     def get_row(self, r):
